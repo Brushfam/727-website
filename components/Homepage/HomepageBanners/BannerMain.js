7cd0412ed2861d3f1856d064098c17f1ac91fc4d
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2022-11-18 15:42:37 +0200

    edit BannerMain

--- components/Homepage/HomepageBanners/BannerMain.js
+++ components/Homepage/HomepageBanners/BannerMain.js
@@ -8,12 +8,14 @@ const BannerMain = () => {
             </div>
 
             <div className={banner.textContent}>
-                <h2 className='header1'>Coming soon</h2>
+                {/*<h2 className='header1'>Coming soon</h2>*/}
                 <div className={banner.rightside}>
-                    <p className='main'>Venture studio that creates, builds, and funds the next generation of web3 companies</p>
+                    <p className='main'>Venture studio that creates, builds, and funds the next generation of web3 companies.</p>
                     <p className='main'></p>
-                    <p className='main'>We are focused on companies that change the landscape of the markets and ecosystems on both small and big scales,
-                        those that drive the creation of web3, and those that are dedicated towards innovation done for people. </p>
+                    <p className='main'>We are a team of visionaries, game-changers, and masterminds who will help you to build an outstanding company. </p>
+                    <p className='main'></p>
+                    <p className='main'>We are focused on companies that change the landscape of the markets and ecosystems
+                        on both small and big scales, those that drive the creation of web3, and those that are dedicated towards innovation done for people.</p>
                 </div>
             </div>
 
