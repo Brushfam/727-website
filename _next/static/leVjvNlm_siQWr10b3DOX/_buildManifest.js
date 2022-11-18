d0dc3b4798795c924327197cee32e1194e30072f
o-tsaruk <90980937+o-tsaruk@users.noreply.github.com>
2022-11-18 13:43:30 +0000

    Deploying to gh-pages from @ 727-Ventures/727-website@7cd0412ed2861d3f1856d064098c17f1ac91fc4d 🚀

---
+++ _next/static/leVjvNlm_siQWr10b3DOX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/240cc5959942bf2f.css","static/chunks/pages/index-5d7fc706eb37b4af.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
